1618a0e7f0de937812153f444fb0c5266a43e1fd
jiangwei <jiangwei@hualala.com>
2020-05-02 21:40:43 +0800

    fisrt commit

---
+++ euraka-client-peer1/src/main/java/com/didala/jiange/eureka/eurakaclientpeer1/EurakaClientPeer1Application.java
@@ -0,0 +1,13 @@
+package com.didala.jiange.eureka.eurakaclientpeer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakaClientPeer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurakaClientPeer1Application.class, args);
+    }
+
+}
---
+++ euraka-client-peer1/src/test/java/com/didala/jiange/eureka/eurakaclientpeer1/EurakaClientPeer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didala.jiange.eureka.eurakaclientpeer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaClientPeer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
